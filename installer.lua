ed7d2c1f39f02a5289549e8bf69f879b59743369
Meow-git-hub <152054092+Meow-git-hub@users.noreply.github.com>
2025-04-29 10:24:11 +0300

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -5,5 +5,4 @@ shell.run("wget https://raw.githubusercontent.com/Meow-git-hub/tank-game/refs/he
 print("Installing allLevels.txt")
 shell.run("wget https://raw.githubusercontent.com/Meow-git-hub/tank-game/refs/heads/main/allLevels.txt allLevels.txt")
 print("Done!")
-os.sleep(2)
 os.run("exit")
